abfebf17e49e1a59363c3e278e2646f3e37e0a93
julio <julio.prog124@gmail.com>
2024-04-18 15:52:04 -0300

    feat: initial structure created

---
+++ main.py
@@ -0,0 +1,5 @@
+from service.FunctionService import FunctionService
+
+if __name__ == "__main__":
+
+    FunctionService.soma(self="")
---
+++ service/FunctionService.py
@@ -0,0 +1,27 @@
+class FunctionService:
+    def soma(self):
+        print("Teste soma")
+        pass
+    def subtraction(self):
+        pass
+
+    def multiplication(self):
+        pass
+
+    def potentiation(self):
+        pass
+
+    def factorial(self):
+        pass
+
+    def roofDivision(self):
+        pass
+
+    def floorDivision(self):
+        pass
+
+    def percentage(self):
+        pass
+
+    def module(self):
+        pass
