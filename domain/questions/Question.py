ce651b9c123dc36fd5fbe3fc4405a53422bc009a
julio <julio.prog124@gmail.com>
2024-07-29 21:47:51 -0300

    refactor: refactored method validate_answer

--- domain/questions/Question.py
+++ domain/questions/Question.py
@@ -7,41 +7,32 @@ class Question:
         self.answer = answer
 
 
-    #Tem um modo mais otimizado, porem precisavamos utilizar as funcoes recursivas criadas por nós, rsrs :(
     def validate_answer(self, user_answer):
-
         functionService = FunctionService()
+
         if isinstance(self.answer, int):
             return int(user_answer) == self.answer
 
-        if self.function == 'sum':
-            return user_answer.lower() == functionService.sum(self.values)
-        if self.function == 'subtraction':
-            return user_answer.lower() == functionService.subtraction(self.values)
-        if self.function == 'multiplication':
-            return user_answer.lower() == functionService.multiplication(self.values)
-        if self.function == 'pow':
-            return user_answer.lower() == functionService.pow(self.values)
-        if self.function == 'factorial':
-            return user_answer.lower() == functionService.factorial(self.values)
-        if self.function == 'roofDivison':
-            return user_answer.lower() == functionService.roofDivision(self.values)
-        if self.function == 'floorDivison':
-            return user_answer.lower() == functionService.floorDivision(self.values)
-        if self.function == 'percentage':
-            return user_answer.lower() == functionService.percentage(self.values)
-        if self.function == 'modulus':
-            return user_answer.lower() == functionService.modulus(self.values)
-        if self.function == 'max':
-            return user_answer.lower() == max(self.values)
-        if self.function == 'min':
-            return user_answer.lower() == min(self.values)
+        function_map = {
+            'sum': functionService.sum,
+            'subtraction': functionService.subtraction,
+            'multiplication': functionService.multiplication,
+            'pow': functionService.pow,
+            'factorial': functionService.factorial,
+            'roofDivision': functionService.roofDivision,
+            'floorDivision': functionService.floorDivision,
+            'percentage': functionService.percentage,
+            'modulus': functionService.modulus,
+            'max': max,
+            'min': min
+        }
+
+        if self.function in function_map:
+            result = function_map[self.function](self.values)
+            return user_answer.lower() == result
+
         if self.function == 'none':
             return user_answer.lower() == self.answer
-
-
-
-
 
     def __repr__(self):
         return f"Question(question='{self.question}', function='{self.function}', values={self.values}, answer='{self.answer}')"
