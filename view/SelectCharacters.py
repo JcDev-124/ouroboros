be2dee1b9a909d8219e33c543f6954cdbfa3d5e3
pedzero <pedro9669silva@gmail.com>
2024-08-08 23:05:58 -0300

    fix: correct draw characters and restyle

--- view/SelectCharacters.py
+++ view/SelectCharacters.py
@@ -49,10 +49,6 @@ class SelectCharacters(BaseView):
             self.matchService.getPlayers()[self.indexPlayer].setCharacter(selectedCharacter)
             self.indexesSprite[self.indexPlayer] = self.indexCharacter
 
-            character = self.matchService.getPlayers()[self.indexPlayer].getCharacter()
-            self._drawImage(character.getSprite(), (250, 250), (self.x, 450))
-            self._drawButton(str(self.indexPlayer + 1), self._mainFont, 10, Colors.BLACK, (self.x + 100, 630), (40, 40))
-            self.x += 250
             self.indexPlayer += 1
 
     def startMatch(self):
@@ -60,7 +56,6 @@ class SelectCharacters(BaseView):
             self.checkSelectedPlayers()
         except ValueError as e:
             self._drawText(e, self.fontSize + 2, self._mainFont, Colors.WHITE, (640, 50))
-
             return
 
         MatchView().run(self.matchService)
@@ -72,30 +67,79 @@ class SelectCharacters(BaseView):
             raise ValueError(message)
 
     def run(self):
-        self._setCaption("Match")
-        gameBarSize = (self._screenWidth, 220)
-        gameBarSurface = pygame.Surface(gameBarSize)
-        gameBarSurface.fill(Colors.CYAN)
-        self._screen.blit(gameBarSurface, (0, (self._screenHeight - gameBarSize[1])))
-        self._screen.fill(Colors.BLACK)
+        self._bg_frame_interval = 8
+        self._ch_frame_interval = 3
         while self._running:
+            clock = pygame.time.Clock()
+            fps = 60
 
             self._drawText('Seleção de Jogadores', self.fontSize + 2, self._mainFont, Colors.WHITE, (640, 50))
-            self._drawCharacter('./assets/champion-selection/background.gif', (1280, 500), (0, 0))
-
+            self._drawBackground('./assets/images/backgrounds/characterSelectionBackground.gif', (self._screenWidth, 490), (0, 0))
+            self._drawImage('./assets/images/buttons/menuButton.png', (self._screenWidth, 230), (0, self._screenHeight - 230))
 
             if self.indexPlayer < len(self.matchService.getPlayers()):
-                self._drawCharacter(self.characters[self.indexCharacter].getSprite(), (350, 350), (450, 225))
-                self._drawButton('<', self._mainFont, self.fontSize, Colors.LIGHT_GRAY, (525, 460), (48, 40), None,
-                                 self.previousCharacter)
-                self._drawButton(str(1), self._mainFont, self.fontSize, Colors.LIGHT_GRAY, (600, 460), (48, 40),
-                                 None, self.selectCharacter)
-                self._drawButton('>', self._mainFont, self.fontSize, Colors.LIGHT_GRAY, (675, 460), (48, 40), None,
-                                 self.nextCharacter)
+                self.drawCharacterOption(self.indexPlayer + 1)
             else:
-                self._drawButton('Iniciar partida', self._mainFont, self.fontSize, Colors.LIGHT_GRAY, (525, 675),
-                                 (200, 50), None,
-                                 self.startMatch)
+                self.drawStartMatchButton()
+
+            self.drawSelectedCharacters()
             self._event()
 
             pygame.display.update()
+            clock.tick(fps)
+
+    def drawCharacterOption(self, index):
+        # change if needed
+        topGap = 270
+        miniatureSize = (200, 200)
+        buttonGap = 10
+        buttonCorrection = 20
+        buttonImage = './assets/images/buttons/menuButton.png'
+
+        # do not change
+        dynamicButtonSize = int(((miniatureSize[0] - (buttonGap * 2)) / 3))
+        buttonSize = (dynamicButtonSize, int(dynamicButtonSize) * 0.6)
+
+        position = ((self._screenWidth / 2) - ((miniatureSize[0] / 2)), topGap)
+        buttonPosition = (position[0], position[1] + miniatureSize[1] - buttonCorrection)
+
+        self._drawCharacter(self.characters[self.indexCharacter].getSprite(), miniatureSize, position)
+        self._drawButton(u'\u2190', self._mainFont, self.fontSize, Colors.BLACK, buttonPosition, buttonSize, buttonImage, self.previousCharacter)
+        buttonPosition = ((buttonPosition[0] + buttonSize[0] + buttonGap), buttonPosition[1])
+        self._drawButton(str(index), self._mainFont, self.fontSize, Colors.BLACK, buttonPosition, buttonSize, buttonImage, self.selectCharacter)
+        buttonPosition = ((buttonPosition[0] + buttonSize[0] + buttonGap), buttonPosition[1])
+        self._drawButton(u'\u2192', self._mainFont, self.fontSize, Colors.BLACK, buttonPosition, buttonSize, buttonImage, self.nextCharacter)
+
+    def drawSelectedCharacters(self):
+        # change if needed
+        gapBetween = 10
+        miniatureSize = (180, 180)
+        barSize = (self._screenWidth, 230)
+
+        # fixed variables
+        players = self.matchService.getPlayers()
+        numberOfPlayers = len(players)
+
+        totalWidth = (numberOfPlayers * miniatureSize[0]) + ((numberOfPlayers - 1) * gapBetween)
+        position = ((self._screenWidth - totalWidth) / 2, (self._screenHeight - barSize[1] + ((barSize[1] - miniatureSize[1]) / 2)))
+
+        for i in range(numberOfPlayers):
+            if i > self.indexPlayer:
+                break
+
+            character = players[i].getCharacter()
+            if character:
+                self._drawCharacter(character.getSprite(), miniatureSize, position)
+                position = ((position[0] + gapBetween + miniatureSize[0]), position[1])
+
+    def drawStartMatchButton(self):
+        # change if needed
+        offSet = -15
+        topGap = 418
+        buttonSize = (160, 55)
+        buttonImage = './assets/images/buttons/menuButton.png'
+
+        # do not change
+        position = ((self._screenWidth / 2) - (buttonSize[0] / 2) + offSet, topGap)
+
+        self._drawButton('Iniciar partida', self._mainFont, self.fontSize, Colors.BLACK, position, buttonSize, buttonImage, self.startMatch)
