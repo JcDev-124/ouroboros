61a830c509e4932aa8e4a0d9a44067bb6eceb43f
julio <julio.prog124@gmail.com>
2024-08-11 19:00:58 -0300

    fix: treat user input

--- domain/questions/Question.py
+++ domain/questions/Question.py
@@ -6,12 +6,17 @@ class Question:
         self.values = values
         self.answer = answer
 
-
     def validate_answer(self, user_answer):
         functionService = FunctionService()
 
         if isinstance(self.answer, int):
             return str(user_answer) == str(self.answer)
+
+        elif isinstance(self.answer, str):
+            if isinstance(user_answer, str):
+                return user_answer[0].lower() == self.answer[0].lower()
+            else:
+                return False
 
         function_map = {
             'sum': functionService.sum,
@@ -23,16 +28,16 @@ class Question:
             'floorDivision': functionService.floorDivision,
             'percentage': functionService.percentage,
             'modulus': functionService.modulus,
-            'max': max,
-            'min': min
+            'max': functionService.max,
+            'min': functionService.min
         }
 
         if self.function in function_map:
             result = function_map[self.function](self.values)
-            return user_answer.lower() == result
+            return functionService.equal(user_answer.lower(), result)
 
         if self.function == 'none':
-            return user_answer.lower() == self.answer
+            return functionService.equal(user_answer.lower(), self.answer)
 
     def __repr__(self):
         return f"Question(question='{self.question}', function='{self.function}', values={self.values}, answer='{self.answer}')"
