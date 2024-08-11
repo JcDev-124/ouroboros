ce51ecc177ad7c5472208de3d92152acdcbd3e55
julio <julio.prog124@gmail.com>
2024-08-11 17:28:44 -0300

    fix: fixed player selection error

--- service/MatchService.py
+++ service/MatchService.py
@@ -7,6 +7,7 @@ states = {
     'selectingAttack': 2,
     'waitingAnswer': 3,
     'attacking': 4,
+    'end': 5
 }
 
 class MatchService:
@@ -39,7 +40,7 @@ class MatchService:
 
     def validateAnswer(self, question, answer, damage, defender, punish, typeAttack, ultimate=False):
         if question.validate_answer(answer):
-            if not isinstance(self.players[self.getAttackerIndex()].getCharacter(), CharacterDamage) and ultimate:
+            if not isinstance(self.getPlayer(self.getAttackerIndex()).getCharacter(), CharacterDamage) and ultimate:
                 typeAttack()
             else:
                 typeAttack(damage, defender.getCharacter())
@@ -71,6 +72,14 @@ class MatchService:
     def getPlayers(self):
         return self.players
 
+    def getPlayer(self, index):
+        if len(self.players) == 1:
+            return self.players[0]
+        elif len(self.players) == 0:
+            return self.attacker
+
+        return self.players[index]
+
     def setCurrentState(self, state):
         self.currentState = state
 
@@ -78,8 +87,10 @@ class MatchService:
         return self.currentState
 
     def getAttackerIndex(self):
-        if self.attacker not in self.players:
+        if self.attacker not in self.players and len(self.players) > 1:
             self.attacker = random.choice(self.players)
+        elif len(self.players) == 1:
+            return 0
         return self.players.index(self.attacker)
 
     def __setNextAttacker(self):
@@ -87,8 +98,12 @@ class MatchService:
         currentAttackerIndex = self.getAttackerIndex()
         if (maxIndex) == currentAttackerIndex:
             self.attacker = self.players[0]
+        elif len(self.players) == 1:
+            pass
         else:
             self.attacker = self.players[currentAttackerIndex + 1]
+
+
 
     def __willEliminate(self):
         for player in self.players:
@@ -102,4 +117,8 @@ class MatchService:
         self.__setNextAttacker()
 
     def gameFinished(self):
-        return len(self.players) == 1
+        if len(self.players) == 1:
+            self.currentState = states['end']
+            return True
+        return False
+
